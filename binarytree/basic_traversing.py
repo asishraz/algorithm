fd82c35bede09355666a71f6be9351d6e8afdfd2
asish <iamasish3001@gmail.com>
2020-07-15 08:30:23 +0530

    learning travesing of binary programming

--- binarytree/basic_traversing.py
+++ binarytree/basic_traversing.py
@@ -1,3 +1,5 @@
+'''
+
 class Node:
     def __init__(self,value):
         self.value = value
@@ -24,4 +26,28 @@ tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
+'''
+##################################################################
+
+class Node:
+     def __init__(self,value):
+          self.value = value
+          self.left = None
+          self.right = None
+
+
+class BinaryTree:
+     def __init__(self,root):
+          self.root = Node(root)
+          
+
+
+tree = BinaryTree()
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+
 
