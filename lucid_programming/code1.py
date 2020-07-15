be298ee7d1d72d3bd5838fe2c9ab90f4a3bdc71a
asish <iamasish3001@gmail.com>
2020-07-15 08:54:50 +0530

    linear search code added

--- lucid_programming/code1.py
+++ lucid_programming/code1.py
@@ -2,10 +2,16 @@ ls = [2,3,33,11,53,1,41,53,32,42,23,42,4,1,3,4,24,34,45,43]
 
 #if target is in the above list or not
 
-target = 25
+target = 53
 
 #linear search
-
-
 def linearSearch(ls,target):
-    
+    for i in range(len(ls)):
+        if ls[i] == target:
+            print(str(target) + " is in the list ")
+            return True
+    return False
+
+
+
+print(linearSearch(ls,target))
