7ce9ba72b72bc74937af3aa1bac8e5a242376c49
asish <iamasish3001@gmail.com>
2020-07-15 09:09:16 +0530

    learnt binary search for finding the number in list

--- lucid_programming/code1.py
+++ lucid_programming/code1.py
@@ -2,7 +2,7 @@ ls = [2,3,33,11,53,1,41,53,32,42,23,42,4,1,3,4,24,34,45,43]
 
 #if target is in the above list or not
 
-target = 53
+target = 44
 
 #linear search
 def linearSearch(ls,target):
@@ -11,7 +11,25 @@ def linearSearch(ls,target):
             print(str(target) + " is in the list ")
             return True
     return False
+##print(linearSearch(ls,target))
 
 
 
-print(linearSearch(ls,target))
+#binary search
+def binarySearch(ls,target):
+    ls.sort()
+    print(ls)
+    low = 0
+    high = len(ls)-1
+    while low <= high:        
+        mid = (low+high) // 2
+        if target == ls[mid]:
+            return True
+        elif target < ls[mid]:
+            high = mid - 1
+        elif target > ls[mid]:
+            low = mid + 1
+    return False
+
+
+print(binarySearch(ls,target))
